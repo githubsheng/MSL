050ca98aa9369033aebf3bfd2a916f97fb663ef8
wang sheng <sheng.wang@rakuten.com>
2017-09-22 17:27:07 +0900

    add two built in command node to tell vm to communicate with outside

---
+++ src/main/java/DLS/ASTNodes/statement/built/in/commands/SendDataNode.java
@@ -0,0 +1,16 @@
+package DLS.ASTNodes.statement.built.in.commands;
+
+import DLS.ASTNodes.statement.ExpressionNode;
+import DLS.ASTNodes.statement.StatementNode;
+import java.util.List;
+
+/**
+ * Created by sheng.wang on 2017/09/22.
+ */
+public class SendDataNode extends StatementNode {
+    private final List<? extends ExpressionNode> data;
+
+    public SendDataNode(List<? extends ExpressionNode> data) {
+        this.data = data;
+    }
+}
