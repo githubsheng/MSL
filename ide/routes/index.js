84053d6ceda03355ba8a8ef255d2a41933256c7b
wang sheng <sheng.wang@rakuten.com>
2017-10-18 19:29:11 +0900

    compiler bridege: make path platform independent

--- ide/routes/index.js
+++ ide/routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const path = require('path');
 const indexFilePath = path.resolve('public/html/index.html');
 const {execSync} = require('child_process');
+const slash = require('slash');
+const {rootPath} = require('../util');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -11,7 +13,11 @@ router.get('/', function (req, res, next) {
 
 router.post('/exec', function (req, res, next) {
     console.log(req.body.data);
-    const child = execSync('java -jar D:/personal/MSL/ide/MSL-1.0-SNAPSHOT-jar-with-dependencies.jar D:/personal/MSL/ide/MSLtest/input.txt D:/personal/MSL/ide/MSLtest');
+    const jarPath = slash(path.resolve(rootPath, "MSL-1.0-SNAPSHOT-jar-with-dependencies.jar"));
+    const inputPath = slash(path.resolve(rootPath, "compiler/input/input.txt"));
+    const outputPath = slash(path.resolve(rootPath, "compiler/output"));
+    console.log(`java -jar ${jarPath} ${inputPath} ${outputPath}`);
+    const child = execSync(`java -jar ${jarPath} ${inputPath} ${outputPath}`);
     console.log(`child process exited with code ${child.status} and signal ${child.signal}`);
 
     res.send("1");
---
+++ ide/util.js
@@ -0,0 +1,6 @@
+/**
+ * Created by sheng.wang on 2017/10/18.
+ */
+module.exports = {
+    rootPath: __dirname
+};
