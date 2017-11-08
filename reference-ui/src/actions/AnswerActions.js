f1c5fc3d2db6adf856d98913810537c4d1806ec0
wang sheng <sheng.wang@rakuten.com>
2017-11-08 18:17:46 +0900

    answer actions

--- reference-ui/src/actions/AnswerActions.js
+++ reference-ui/src/actions/AnswerActions.js
@@ -4,9 +4,38 @@
 
 const prefix = "answer_";
 export const actionTypeSubmitAnswer = `${prefix}submitAnswers`;
+export const actionTypeSelectRow = `${prefix}selectRow`;
+export const actionTypeSelectCol = `${prefix}selectCol`;
+export const actionTypeTextInput = `${prefix}textInput`;
 
 export function submitAnswersAction(){
     return {
         type: actionTypeSubmitAnswer
+    }
+}
+
+export function selectRow(questionId, rowId){
+    return {
+        type: actionTypeSelectRow,
+        questionId,
+        rowId
+    }
+}
+
+export function selectCol(questionId, rowId, colId) {
+    return {
+        type: actionTypeSelectCol,
+        questionId,
+        rowId,
+        colId
+    }
+}
+
+export function selectTextInput(questionId, inputId, value) {
+    return {
+        type: actionTypeTextInput,
+        questionId,
+        inputId,
+        value
     }
 }
