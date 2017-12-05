d3816c573ce2113077417d8df56425cfe1ca1e49
githubsheng <wang.sheng.unique@gmail.com>
2017-12-06 00:05:56 +0900

    now correctly copy the updated css from rui to ide

--- reference-ui/packageAndCopyToIde.sh
+++ reference-ui/packageAndCopyToIde.sh
@@ -5,7 +5,7 @@ rm ../ide/public/javascripts/reference_ui/*
 rm ../ide/public/css/reference_ui/*
 
 cp -r ./build/static/js/* ../ide/public/javascripts/reference_ui/
-cp -r ./build/static/css/* css/reference_ui/
+cp -r ./build/static/css/* ../ide/public/css/reference_ui/
 
 mv ../ide/public/javascripts/reference_ui/*.js ../ide/public/javascripts/reference_ui/rui.js
-mv ../ide/public/css/reference_ui/*.css ../ide/public/css/reference_ui/rui.js
+mv ../ide/public/css/reference_ui/*.css ../ide/public/css/reference_ui/rui.css
