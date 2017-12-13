1501a117a239a4899ba4dee7028d5b4d55212c50
wang sheng <sheng.wang@rakuten.com>
2017-12-13 18:55:47 +0900

    refactoring the razer plugin code to adapt to new lifecycle events

--- ide/public/temp_msl_plugins_repo/razer.js
+++ ide/public/temp_msl_plugins_repo/razer.js
@@ -1,24 +1,38 @@
 (function () {
 
     function razer(action) {
-        console.log(action);
-        if (action.type === "plugin_questionChanged") {
-            const question = action.question;
-            if (question.razer === "true") {
-                //mount the razer iframe....
-                const iframeElem = document.createElement("iframe");
-                iframeElem.height = "528";
-                iframeElem.scrolling = "no";
-                iframeElem.src = "//assets.razerzone.com/animation/ouroboros_v2/index.html";
-                iframeElem.width = "938";
-                iframeElem.classList.add("razer-iframe");
 
-                const belowQuestionTextDiv = action.questionDiv.querySelector(".below-question-text");
-                belowQuestionTextDiv.appendChild(iframeElem);
-            } else {
-                const iframe = action.questionDiv.querySelector(`.below-question-text .razer-iframe`);
-                if (iframe) iframe.remove();
-            }
+        switch (action.type) {
+            case "plugin_questionLoad":
+                questionLoad(action);
+                break;
+            case "plugin_questionUnLoad":
+                questionUnload(action);
+                break;
+            default:
+            //nothing
+        }
+
+        function questionLoad(action) {
+            const question = action.question;
+            if(question.razer !== "true") return;
+            //mount the razer iframe....
+            const iframeElem = document.createElement("iframe");
+            iframeElem.height = "528";
+            iframeElem.scrolling = "no";
+            iframeElem.src = "//assets.razerzone.com/animation/ouroboros_v2/index.html";
+            iframeElem.width = "938";
+            iframeElem.classList.add("razer-iframe");
+
+            const belowQuestionTextDiv = action.questionDiv.querySelector(".below-question-text");
+            belowQuestionTextDiv.appendChild(iframeElem);
+        }
+
+        function questionUnload(action) {
+            const question = action.question;
+            if(question.razer !== "true") return;
+            const iframe = action.questionDiv.querySelector(`.below-question-text .razer-iframe`);
+            if (iframe) iframe.remove();
         }
     }
 
