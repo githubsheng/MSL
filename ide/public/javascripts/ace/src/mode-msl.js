a921f70558c3accdd203f4fee7781c50344cacfd
wang sheng <sheng.wang@rakuten.com>
2017-12-19 14:07:02 +0900

    ide syntax highlight support for in script row tags and col tags

--- ide/public/javascripts/ace/src/mode-msl.js
+++ ide/public/javascripts/ace/src/mode-msl.js
@@ -86,6 +86,31 @@ define("ace/mode/msl_highlight_rules", ["require", "exports", "module", "ace/lib
             next: "pageAndPageGroupStart"
         };
 
+        const PreScriptRowTagRule = {
+            token: "question_row",
+            regex: /\[Row/,
+            next: 'preScriptRowTag'
+        };
+
+        const PreScriptColTagRule = {
+            token: "question_col",
+            regex: /\[Col/,
+            next: 'preScriptColTag'
+        };
+
+        const PostScriptRowTagRule = {
+            token: "question_row",
+            regex: /\[Row/,
+            next: 'postScriptRowTag'
+        };
+
+        const PostScriptColTagRule = {
+            token: "question_col",
+            regex: /\[Col/,
+            next: 'postScriptColTag'
+        };
+
+
         this.$rules = {
             "start": startRules,
 
@@ -113,6 +138,8 @@ define("ace/mode/msl_highlight_rules", ["require", "exports", "module", "ace/lib
                 StringRule,
                 BooleanRule,
                 DotRule,
+                PreScriptRowTagRule,
+                PreScriptColTagRule,
                 KeyWordMapperRule,
                 QuestionStartRule,
                 /*
@@ -164,7 +191,7 @@ define("ace/mode/msl_highlight_rules", ["require", "exports", "module", "ace/lib
                 submitButtonRule,
                 PageEndRule],
 
-            "inPostQuestionScript": [StringRule, BooleanRule, DotRule, KeyWordMapperRule, PageEndRule],
+            "inPostQuestionScript": [StringRule, BooleanRule, DotRule, PostScriptRowTagRule, PostScriptColTagRule, KeyWordMapperRule, PageEndRule],
 
             "inRowTag": [StringRule, EqualSignRule, {
                 token: "question_row",
@@ -176,6 +203,54 @@ define("ace/mode/msl_highlight_rules", ["require", "exports", "module", "ace/lib
                 token: "question_col",
                 regex: /]/,
                 next: "inQuestionBody"
+            }],
+
+            "preScriptRowTag": [StringRule, EqualSignRule, {
+                token: "question_row",
+                regex: /]/,
+                next: "preScriptRowEndTag"
+            }],
+
+            "preScriptColTag": [StringRule, EqualSignRule, {
+                token: "question_col",
+                regex: /]/,
+                next: "preScriptColEndTag"
+            }],
+
+            "preScriptRowEndTag": [{
+                token: "question_row",
+                regex: /\[End]/,
+                next: "inPreQuestionScript"
+            }],
+
+            "preScriptColEndTag": [{
+                token: "question_col",
+                regex: /\[End]/,
+                next: "inPreQuestionScript"
+            }],
+
+            "postScriptRowTag": [StringRule, EqualSignRule, {
+                token: "question_row",
+                regex: /]/,
+                next: "postScriptRowEndTag"
+            }],
+
+            "postScriptColTag": [StringRule, EqualSignRule, {
+                token: "question_col",
+                regex: /]/,
+                next: "postScriptColEndTag"
+            }],
+
+            "postScriptRowEndTag": [{
+                token: "question_row",
+                regex: /\[End]/,
+                next: "inPostQuestionScript"
+            }],
+
+            "postScriptColEndTag": [{
+                token: "question_col",
+                regex: /\[End]/,
+                next: "inPostQuestionScript"
             }]
 
         };
